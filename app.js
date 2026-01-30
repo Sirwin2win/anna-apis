6877b9144abffe9075cca1bbe0c49e1a3e3c9f1f
Sirwin <sirwin2win@gmail.com>
2026-01-30 11:34:54 +0100

    static

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express()
 connectDB()
 // middlewares
 app.use(cors())
+app.use(express.static('upload'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/',produuctRoutes)
